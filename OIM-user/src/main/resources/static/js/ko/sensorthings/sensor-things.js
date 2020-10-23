0b90724a1cfb7060feb3358911bc05012fe39f28
yhjeong1009 <yhjeong@gaia3d.com>
2020-10-23 14:55:52 +0900

    사물인터넷 미세먼지 목록, 상세 정보 보기

--- OIM-user/src/main/resources/static/js/ko/sensorthings/sensor-things.js
+++ OIM-user/src/main/resources/static/js/ko/sensorthings/sensor-things.js
@@ -1,13 +1,8 @@
 const SensorThings = function (magoInstance) {
-
     this.magoInstance = magoInstance;
     this.FROST_SERVER_URL = 'http://localhost:8888/FROST-Server/v1.0/';
     this.queryString = '';
     this.type = 'iot_occupancy'; // iot_occupancy, iot_dust
-
-    // http://localhost:8888/FROST-Server/v1.0/ObservedProperties(1)/Datastreams?$select=@iot.id,description,unitOfMeasurement&$expand=Observations($select=result,phenomenonTime;$orderby=phenomenonTime%20desc;$top=1;$count=true),Thing($select=@iot.id,name,description;$expand=Locations($select=location,name))
-    // http://localhost:8888/FROST-Server/v1.0/Locations?$select=@iot.id,location,name&$top=5&$count=true&$expand=Things($select=@iot.id,name,description),Things/Datastreams($select=@iot.id,description;$filter=ObservedProperty/name eq '미세먼지(PM10) Particulates'),Things/Datastreams/Observations($select=result,phenomenonTime;$orderby=phenomenonTime desc;$top=1)
-
 };
 
 
@@ -22,7 +17,7 @@ SensorThings.prototype.dataSearch = function (pageNo) {
     if (this.type === 'iot_occupancy') {
         this.occupancyList(pageNo, params);
     } else if (this.type === 'iot_dust') {
-        this.dustList(pageNo, params);
+        dustSensorThings.dustList(pageNo, params);
     }
 
 };
@@ -31,17 +26,17 @@ SensorThings.prototype.occupancyList = function (pageNo, params) {
     const _this = this;
     $.ajax({
         url: _this.FROST_SERVER_URL +
-            'Locations?$select=@iot.id,location,name&$top=1&$count=true&$expand=' +
+            'Locations?$select=@iot.id,location,name&$top=5&$count=true&$orderby=name asc&$expand=' +
             'Things($select=@iot.id,name,description),' +
-            'Things/Datastreams($select=@iot.id,description,unitOfMeasurement;$filter=ObservedProperty/name eq \'미세먼지(PM10) Particulates\'),' +
-            'Things/Datastreams/Observations($select=result,phenomenonTime;$orderby=phenomenonTime desc;$top=1)',
+            'Things/Datastreams($select=@iot.id,description,unitOfMeasurement;$filter=ObservedProperty/name eq \'pm10Value\'),' +
+            'Things/Datastreams/Observations($select=result,phenomenonTime,resultTime;$orderby=resultTime desc;$top=1)',
         type: "GET",
         dataType: "json",
         headers: {"X-Requested-With": "XMLHttpRequest"},
         success: function (msg) {
             console.info(msg);
 
-            const pagination = new Pagination(pageNo, msg['@iot.count'], 1, msg['@iot.nextLink']);
+            const pagination = new Pagination(pageNo, msg['@iot.count'], 5, msg['@iot.nextLink']);
             msg.pagination = pagination;
 
             const templateLegend = Handlebars.compile($("#iotLegendSource").html());
@@ -57,43 +52,6 @@ SensorThings.prototype.occupancyList = function (pageNo, params) {
             $("#iotPaginationDHTML").html("").append(templatePagination(msg));
 
             $('#iotOccupancyListDHTML').show();
-        },
-        error: function (request, status, error) {
-            alert(JS_MESSAGE["ajax.error.message"]);
-        }
-    });
-};
-
-SensorThings.prototype.dustList = function (pageNo, params) {
-    const _this = this;
-    $.ajax({
-        url: _this.FROST_SERVER_URL +
-            'Locations?$select=@iot.id,location,name&$top=1&$count=true&$expand=' +
-            'Things($select=@iot.id,name,description),' +
-            'Things/Datastreams($select=@iot.id,description,unitOfMeasurement;$filter=ObservedProperty/name eq \'미세먼지(PM10) Particulates\'),' +
-            'Things/Datastreams/Observations($select=result,phenomenonTime;$orderby=phenomenonTime desc;$top=1)',
-        type: "GET",
-        dataType: "json",
-        headers: {"X-Requested-With": "XMLHttpRequest"},
-        success: function (msg) {
-            console.info(msg);
-
-            const pagination = new Pagination(pageNo, msg['@iot.count'], 1, msg['@iot.nextLink']);
-            msg.pagination = pagination;
-
-            const templateLegend = Handlebars.compile($("#iotLegendSource").html());
-            $("#iotLegendDHTML").html("").append(templateLegend(params));
-
-            const templateSearchSummary = Handlebars.compile($("#searchSummarySource").html());
-            $("#iotSearchSummaryDHTML").html("").append(templateSearchSummary(msg));
-
-            const template = Handlebars.compile($("#dustListSource").html());
-            $("#iotDustListDHTML").html("").append(template(msg));
-
-            const templatePagination = Handlebars.compile($("#paginationSource").html());
-            $("#iotPaginationDHTML").html("").append(templatePagination(msg));
-
-            $('#iotDustListDHTML').show();
         },
         error: function (request, status, error) {
             alert(JS_MESSAGE["ajax.error.message"]);
--- OIM-user/src/test/java/io/openindoormap/domain/LangTest.java
+++ OIM-user/src/test/java/io/openindoormap/domain/LangTest.java
@@ -8,4 +8,19 @@ public class LangTest {
     void 언어_변환() {
 
     }
+
+    @Test
+    void 페이징_테스트() {
+        Long pageNo = 2L;
+        Long pageListCount = 5L;
+        Long startPage = ((pageNo - 1L) / pageListCount) * pageListCount + 1L;
+        Long endPage = ((pageNo - 1L) / pageListCount) * pageListCount + pageListCount;
+
+        System.out.println((pageNo - 1L));
+        System.out.println((pageNo - 1L) / pageListCount);
+        System.out.println(((pageNo - 1L) / pageListCount) * pageListCount);
+        System.out.println(((pageNo - 1L) / pageListCount) * pageListCount + 1L);
+
+    }
+
 }
